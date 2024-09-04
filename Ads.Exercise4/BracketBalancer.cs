95b6946f1f0946893c9996614d8225fc31464197
nekix <nekixxiken@gmail.com>
2024-09-04 13:42:09 +0300

    Implement BracketBalancer.

---
+++ Ads.Exercise4/BracketBalancer.cs
@@ -0,0 +1,53 @@
+﻿using AlgorithmsDataStructures;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.Exercise4
+{
+    public static class BracketBalancer
+    {
+        public static bool CheckIsBalanced(string input)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                switch (ch)
+                {
+                    case '(':
+                        stack.Push(ch);
+                        break;
+                    case ')':
+                        if(stack.Size() == 0) return false;
+                        stack.Pop();
+                        break;
+                }
+            }
+
+            return stack.Size() == 0;
+        }
+
+        public static bool AdvancedCheckIsBalanced(string input)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                if (ch == '(' || ch == '[' || ch == '{')
+                    stack.Push(ch);
+                else if (stack.Size() == 0)
+                    return false;
+                else if (ch == ')' && stack.Pop() != '(')
+                    return false;
+                else if (ch == ']' && stack.Pop() != '[')
+                    return false;
+                else if (ch == '}' && stack.Pop() != '{')
+                    return false;
+            }
+
+            return stack.Size() == 0;
+        }
+    }
+}
---
+++ Ads.Tests/Exercise4/BracketBalancerTests/BracketBalancer_CheckIsBalanced_Tests.cs
@@ -0,0 +1,40 @@
+﻿extern alias Exercise4;
+
+using Exercise4.Ads.Exercise4;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ads.Tests.Exercise4.BracketBalancerTests
+{
+    public class BracketBalancer_CheckIsBalanced_Tests
+    {
+        [Theory]
+        [InlineData(true, "((()))")]
+        [InlineData(false, "((())")]
+        [InlineData(true, "(()())")]
+        [InlineData(false, "(()()))")]
+        [InlineData(true, "(()((())()))")]
+        [InlineData(true, "(()()(()))")]
+        public void Should_CheckIsBalanced(bool isBalanced, string input)
+        {
+            BracketBalancer.CheckIsBalanced(input).ShouldBe(isBalanced);
+        }
+
+        [Theory]
+        [InlineData(true, "([({()})])")]
+        [InlineData(false, "((())})")]
+        [InlineData(false, "((){())}")]
+        [InlineData(false, "(()()))")]
+        [InlineData(true, "({()((())[()])})")]
+        [InlineData(true, "(()()(()))")]
+        public void Should_AdvancedCheckIsBalanced(bool isBalanced, string input)
+        {
+            BracketBalancer.AdvancedCheckIsBalanced(input).ShouldBe(isBalanced);
+        }
+    }
+}

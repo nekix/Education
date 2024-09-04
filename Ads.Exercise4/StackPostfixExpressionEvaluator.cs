22fe15b248b5fdf9ac9d527188b3ea45212ffce0
nekix <nekixxiken@gmail.com>
2024-09-04 15:37:54 +0300

    Implement StackPostfixExpressionEvaluator.

---
+++ Ads.Exercise4/StackPostfixExpressionEvaluator.cs
@@ -0,0 +1,61 @@
+﻿using AlgorithmsDataStructures;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.Exercise4
+{
+    public static class StackPostfixExpressionEvaluator
+    {
+        public static double Evaluate(string expression)
+        {
+            var expParts = expression.Split(' ');
+
+            var expressionStack = new Stack<string>();
+            var helpStack = new Stack<double>();
+
+            for(int i = expParts.Length - 1; i >= 0; i--)
+                expressionStack.Push(expParts[i]);
+
+            while(expressionStack.Size() != 0)
+            {
+                var item = expressionStack.Pop();
+
+                if (int.TryParse(item, out var value))
+                {
+                    helpStack.Push(value);
+                    continue;
+                }
+
+                switch (item)
+                {
+                    case "+":
+                        CalculateFromStack(helpStack, (x, y) => x + y);
+                        break;
+                    case "-":
+                        CalculateFromStack(helpStack, (x, y) => x - y);
+                        break;
+                    case "*":
+                        CalculateFromStack(helpStack, (x, y) => x * y);
+                        break;
+                    case "/":
+                        CalculateFromStack(helpStack, (x, y) => x / y);
+                        break;
+                    case "=":
+                        return helpStack.Pop();
+                }
+            }
+
+            return default;
+        }
+
+        private static void CalculateFromStack(Stack<double> stack, Func<double, double, double> func)
+        {
+            var firstValue = stack.Pop();
+            var secondValue = stack.Pop();
+
+            stack.Push(func(firstValue, secondValue));
+        }
+    }
+}
---
+++ Ads.Tests/Exercise4/StackPostfixExpressionEvaluatorTests/StackPostfixExpressionEvaluator_Tests.cs
@@ -0,0 +1,23 @@
+﻿extern alias Exercise4;
+
+using Exercise4.Ads.Exercise4;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ads.Tests.Exercise4.StackPostfixExpressionEvaluatorTests
+{
+    public class StackPostfixExpressionEvaluator_Tests
+    {
+        [Theory]
+        [InlineData(59, "8 2 + 5 * 9 + =")]
+        public void Should_Evaluate(double value, string expression)
+        {
+            StackPostfixExpressionEvaluator.Evaluate(expression).ShouldBe(value);
+        }
+    }
+}

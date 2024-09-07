fc09ec8b0a67d4f07511a2443ae8fe5423f9c5c2
nekix <nekix@DESKTOP-7HDAA7P>
2024-09-07 15:46:18 +0300

    Add projects and base classes for exercise 5.

---
+++ Ads.Exercise5/Queue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Queue<T>
+    {
+        public Queue()
+        {
+            // инициализация внутреннего хранилища очереди
+        }
+
+        public void Enqueue(T item)
+        {
+            // вставка в хвост
+        }
+
+        public T Dequeue()
+        {
+            // выдача из головы
+            return default(T); // если очередь пустая
+        }
+
+        public int Size()
+        {
+            return 0; // размер очереди
+        }
+
+    }
+}
---
+++ Ads.Tests/Exercise_5/Queue_BaseTests.cs
@@ -0,0 +1,15 @@
+﻿extern alias Exercise5;
+
+using Exercise5.AlgorithmsDataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.Tests.Exercise_5
+{
+    public class Queue_BaseTests
+    {
+    }
+}

9ef9f99de09654599f85fa75b979abdedd2593ec
nekix <nekix@DESKTOP-7HDAA7P>
2024-09-12 23:52:02 +0300

    Implement BracketBalancer.

---
+++ Ads.Exercise6/BracketBalancer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmsDataStructures;
+
+namespace Ads.Exercise6
+{
+    public static class BracketBalancer
+    {
+        public static bool CheckIsBalanced(string input)
+        {
+            var deque = new Deque<char>();
+
+            foreach (var ch in input)
+            {
+                if (ch == '(' || ch == '[' || ch == '{')
+                    deque.AddFront(ch);
+                else if (ch == ')' && deque.RemoveFront() != '(')
+                    return false;
+                else if (ch == ']' && deque.RemoveFront() != '[')
+                    return false;
+                else if (ch == '}' && deque.RemoveFront() != '{')
+                    return false;
+            }
+
+            return deque.Size() == 0;
+        }
+    }
+}
---
+++ Ads.Tests/Exercise_6/BracketBalancerTests/BracketBalancer_CheckIsBalanced_Tests.cs
@@ -0,0 +1,29 @@
+﻿extern alias Exercise6;
+using Exercise6::Ads.Exercise6;
+using Shouldly;
+using Xunit;
+
+namespace Ads.Tests.Exercise_6.BracketBalancerTests
+{
+    public class BracketBalancer_CheckIsBalanced_Tests
+    {
+        [Theory]
+        [InlineData(true, "((()))")]
+        [InlineData(false, "((())")]
+        [InlineData(true, "(()())")]
+        [InlineData(false, "(()()))")]
+        [InlineData(true, "(()((())()))")]
+        [InlineData(true, "(()()(()))")]
+        [InlineData(true, "(1 * 5) * 3")]
+        [InlineData(true, "(1 * 5) * 3 + {5 * 6}")]
+        [InlineData(true, "[(15 - 3) * (x + y) * {(12 - 15) / 12}] * [2 - 3]")]
+        [InlineData(false, "(1 * 5) * 3]")]
+        [InlineData(false, "(1 * 5) * 3 + {[5 * 6}")]
+        [InlineData(false, "[(15 - 3) * (x + y) * ((12 - 15) / 12}] * [2 - 3]")]
+        public void Should_CheckIsBalanced(bool isBalanced, string input)
+        {
+            BracketBalancer.CheckIsBalanced(input).ShouldBe(isBalanced);
+        }
+
+    }
+}

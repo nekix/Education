efa780c4e90197d3eba9dfb13bb4649c817657ed
nekix <nekixxiken@gmail.com>
2024-09-12 16:52:05 +0300

    Template for ImprovedDequa.

--- Ads.Exercise6/ImprovedDeque.cs
+++ Ads.Exercise6/ImprovedDeque.cs
@@ -7,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace Ads.Exercise6
 {
-    public class ImprovedDeque<T> : Deque<T>
+    public class ImprovedDeque<T>
     {
+        private List<T> _frontItems;
+        private List<T> _tailItems;
+
+        public ImprovedDeque()
+        {
+            _frontItems = new List<T>();
+            _tailItems = new List<T>();
+        }
+
+        public void AddFront(T item)
+            => _frontItems.Add(item);
+
+        public void AddTail(T item)
+            => _tailItems.Add(item);
+
+        public T RemoveFront()
+        {
+            if(_frontItems.Count == 0)
+        }
+
+        public T RemoveTail()
+        {
+
+        }
     }
 }

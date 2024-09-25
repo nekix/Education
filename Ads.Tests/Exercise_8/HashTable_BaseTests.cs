15a7d22c30e26ca568ec568108756c52f037e82b
nekix <nekix@DESKTOP-7HDAA7P>
2024-09-25 23:04:53 +0300

    Lesson 8. Implement base HashTable and HashFun.

---
+++ Ads.Tests/Exercise_8/HashTable_BaseTests.cs
@@ -0,0 +1,17 @@
+﻿extern alias Exercise8;
+using Exercise8.AlgorithmsDataStructures;
+using AlgorithmsDataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.Tests.Exercise_8
+{
+    public class HashTable_BaseTests
+    {
+        protected static HashTable GetEmptyHashTable(int sz = 17, int stp = 3)
+            => new HashTable(sz, stp);
+    }
+}
---
+++ Ads.Tests/Exercise_8/HashTable_HashFunTests.cs
@@ -0,0 +1,30 @@
+﻿extern alias Exercise8;
+using Exercise8.AlgorithmsDataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace Ads.Tests.Exercise_8
+{
+    public class HashTable_HashFunTests : HashTable_BaseTests
+    {
+        [Theory]
+        [MemberData(nameof(HashFunData))]
+        public void Should_GetHash(string data, int hash)
+        {
+            var hashTable = GetEmptyHashTable(17, 3);
+
+            hashTable.HashFun(data).ShouldBe(hash);
+        }
+
+        public static IEnumerable<object[]> HashFunData =>
+            new List<object[]>
+            {
+                new object[] { "\u0011", "0"},
+            };
+    }
+}

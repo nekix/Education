4cbb89546df7e1e121f603e86bf6bc8bd2506658
nekix <nekixxiken@gmail.com>
2024-10-17 16:49:31 +0300

    Lesson 12. Base implementation.

---
+++ Ads.Exercise12/NativeCache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures
+{
+    public class NativeCache<T>
+    {
+        public int size;
+        public String[] slots;
+        public T[] values;
+        public int[] hits;
+        // ...
+    }
+}
---
+++ Ads.Tests/Exercise12/NativeCache_BaseTests.cs
@@ -0,0 +1,16 @@
+﻿extern alias Exercise12;
+
+using Exercise12.AlgorithmsDataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.Tests.Exercise12
+{
+    public class NativeCache_BaseTests
+    {
+
+    }
+}

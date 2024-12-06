ac305ee64c5fd1f21d1549134bbefd81b08568ce
nekix <nekix@DESKTOP-7HDAA7P>
2024-12-06 22:06:29 +0300

    SQL: Lesson 2. Replace tabs.

--- SQL/Lesson 2.sql
+++ SQL/Lesson 2.sql
@@ -3,95 +3,95 @@ Exercise 1
 */
 
 SELECT
-	name AS SquadName
+    name AS SquadName
 FROM
-	Squads
+    Squads
 WHERE
-	leader_id IS NULL
+    leader_id IS NULL
 
 /*
 Exercise 2
 */
 
 SELECT
-	name AS DwarfName,
-	age AS Age
+    name AS DwarfName,
+    age AS Age
 FROM
-	Dwarves
+    Dwarves
 WHERE
-	age > 150 AND profession = 'Warrior'
+    age > 150 AND profession = 'Warrior'
 
 /*
 Exercise 3
 */
 
 SELECT
-	d.name AS DwarfName,
-	d.age AS Age,
-	d.profession AS Profession
+    d.name AS DwarfName,
+    d.age AS Age,
+    d.profession AS Profession
 FROM
-	Dwarves d
+    Dwarves d
 JOIN
-	Items i
+    Items i
 ON
-	i.type = 'weapon' AND d.dward_id = i.owner_id
+    i.type = 'weapon' AND d.dward_id = i.owner_id
 GROUP BY
-	d.dward_id
-	
+    d.dward_id
+    
 /*
 Exercise 4
 */
 
 SELECT
-	d.name AS DwarfName,
-	d.age AS Age,
-	d.profession AS Profession,
-	t.status as TasksStatus,
-	COUNT(t) as TasksCount
+    d.name AS DwarfName,
+    d.age AS Age,
+    d.profession AS Profession,
+    t.status as TasksStatus,
+    COUNT(t) as TasksCount
 FROM
-	Dwarves d
+    Dwarves d
 JOIN
-	Tasks t
+    Tasks t
 ON
-	d.dwarf_id = t.assigned_to
+    d.dwarf_id = t.assigned_to
 GROUP BY
-	d.dward_id, t.status
-	
+    d.dward_id, t.status
+    
 /*
 Exercise 5
 */
 
 SELECT
-	t.task_id AS TaskId,
-	t.description AS Description,
-	t.status AS Status
+    t.task_id AS TaskId,
+    t.description AS Description,
+    t.status AS Status
 FROM
-	Tasks t
+    Tasks t
 JOIN
-	Dwarves d
+    Dwarves d
 ON
-	t.assigned_to = d.dwarf_id
+    t.assigned_to = d.dwarf_id
 JOIN
-	Squads s
+    Squads s
 ON
-	d.squad_id = s.squad_id
+    d.squad_id = s.squad_id
 WHERE
-	s.name = 'Guardians'
-	
-	
+    s.name = 'Guardians'
+    
+    
 /*
 Exercise 6
 */
 
 SELECT
-	d1.name AS DwarfName,
-	d2.name AS NearRelativeDwarf,
-	d1.relationship AS Relationship
+    d1.name AS DwarfName,
+    d2.name AS NearRelativeDwarf,
+    d1.relationship AS Relationship
 FROM
-	Dwarves d1
+    Dwarves d1
 JOIN
-	Dwarves d2
+    Dwarves d2
 ON
-	d1.rerelated_to = d2.dwarf_id
+    d1.rerelated_to = d2.dwarf_id
 WHERE
-	d1.relationship IN ('Родитель', 'Ребенок', 'Супруг', 'Супруга', 'Брат', 'Сестра', 'Бабушка', 'Дедушка', 'Внук', 'Внучка')
+    d1.relationship IN ('Родитель', 'Ребенок', 'Супруг', 'Супруга', 'Брат', 'Сестра', 'Бабушка', 'Дедушка', 'Внук', 'Внучка')

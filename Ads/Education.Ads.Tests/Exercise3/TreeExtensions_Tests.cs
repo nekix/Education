3a9a33d829ad4b0a0370c3c27f6617610bf20a3f
npetrov <npetrov@advmusic.com>
2025-01-23 17:53:02 +0300

    Предварительный алгоритм

---
+++ Ads/Education.Ads.Tests/Exercise3/TreeExtensions_Tests.cs
@@ -0,0 +1,40 @@
+﻿using AlgorithmsDataStructures2;
+using Education.Ads.Exercise_3;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Education.Ads.Tests.Exercise3
+{
+    public class TreeExtensions_Tests
+    {
+        [Theory]
+        [MemberData(nameof(GetRecoverTreeData))]
+        public void Should_RecoverTree(int[] preorder, int[] inorder, Action<BSTNode> check)
+        {
+            var root = TreeExtensions.RecoverTree(preorder, inorder);
+
+            check.Invoke(root);
+        }
+
+        public static IEnumerable<object[]> GetRecoverTreeData()
+        {
+            int[] preorder = new int[] { 1, 2, 4, 5, 3, 6, 7 };
+            int[] inorder = new int[] { 4, 2, 5, 1, 6, 3, 7 };
+
+            Action<BSTNode> check = (node) =>
+            {
+                node.NodeKey.ShouldBe(1);
+                node.LeftChild.NodeKey.ShouldBe(2);
+                node.LeftChild.LeftChild.NodeKey.ShouldBe(4);
+                node.LeftChild.RightChild.NodeKey.ShouldBe(5);
+                node.RightChild.NodeKey.ShouldBe(3);
+                node.RightChild.LeftChild.NodeKey.ShouldBe(6);
+                node.RightChild.RightChild.NodeKey.ShouldBe(7);
+            };
+
+            yield return new object[] { preorder, inorder, check };
+        }
+    }
+}
---
+++ Ads/Education.Ads/Exercise_3/TreeExtensions.cs
@@ -0,0 +1,58 @@
+﻿using AlgorithmsDataStructures2;
+using System;
+using System.Collections.Generic;
+
+namespace Education.Ads.Exercise_3
+{
+    public static class TreeExtensions
+    {
+        public static BSTNode RecoverTree(int[] preorder, int[] inorder)
+        {
+            if (preorder.Length != inorder.Length)
+                return null;
+
+            int count = preorder.Length;
+
+            if (count == 0)
+                return null;
+
+            if (preorder[count - 1] != inorder[count - 1])
+                return null;
+
+            return RecoverTree(0, preorder.Length, 0, inorder.Length, preorder, inorder);
+        }
+
+        private static BSTNode RecoverTree(int preLeft, int preRigth, int inLeft, int inRight, int[] preorder, int[] inorder)
+        {
+            int root = preorder[preLeft];
+            int inRootIndex = -1;
+
+            for (int i = inLeft; i < inRight; i++)
+            {
+                if (inorder[i] == root)
+                {
+                    inRootIndex = i;
+                    break;
+                }
+            }
+
+            BSTNode node = new BSTNode { NodeKey = root };
+            
+            if (inRootIndex > 0)
+            {
+                BSTNode leftNode = RecoverTree(preLeft + 1, inRootIndex + 1, inLeft, inRootIndex, preorder, inorder);
+                leftNode.Parent = node;
+                node.LeftChild = leftNode;
+            }   
+
+            if (inRootIndex + 1 < inRight - inLeft)
+            {
+                BSTNode rightNode = RecoverTree(inRootIndex + 1, preRigth, inRootIndex + 1, inRight, preorder, inorder);
+                rightNode.Parent = node;
+                node.RightChild = rightNode;
+            }      
+
+            return node;
+        }
+    }
+}

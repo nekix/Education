7fda685f51fa3debfabd7fa3d19b86c66ce726c4
npetrov <npetrov@advmusic.com>
2025-01-28 17:48:01 +0300

    Ads.Part2.Exercise5. Начал реализовывать пятый урок.

---
+++ Ads/Education.Ads/Exercise_5/BalancedBST.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public static class BalancedBST
+    {
+        public static int[] GenerateBBSTArray(int[] a)
+        {
+            Array.Sort(a);
+
+            int size = GetArraySize();
+
+            int[] bbst = new int[size];
+
+            int rootIndex = (a.Length - 1) / 2;
+
+
+
+            return null;
+        }
+
+        private static int[] GenerateBBSTArray(int[] a, )
+
+        private static int GetArraySize(int depth)
+        {
+            return (int)Math.Pow(2, depth + 1) - 1;
+        }
+    }
+}

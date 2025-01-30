e8b27d52a28125d9116babc7d40cd48ae3984ec2
nekix <nekix@DESKTOP-7HDAA7P>
2025-01-30 21:51:15 +0300

    Добавил тесты.

--- Ads/Education.Ads.Tests/Exercise_5/BalancedBST_Tests.cs
+++ Ads/Education.Ads.Tests/Exercise_5/BalancedBST_Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Text;
 using System.Threading.Tasks;
 using AlgorithmsDataStructures2;
@@ -20,8 +21,34 @@ namespace Education.Ads.Tests.Exercise_5
 
         public static IEnumerable<object[]> GetGenerateBBSTArrayData()
         {
-            int[] a = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
-            int[] res = new int[] { 7, 3, 11, 1, 5, 9, 13, 0, 2, 4, 6, 8, 10, 12, 14 };
+            // 1: Пустой массив
+            int[] a = new int[] {  };
+            int[] res = new int[] {  };
+            yield return new object[] { a, res };
+
+            // 2: Один элемент
+            a = new int[] { 7 };
+            res = new int[] { 7 };
+            yield return new object[] { a, res };
+
+            // 3: Древо глубины 1
+            a = new int[] { 12, 7, 1 };
+            res = new int[] { 7, 1, 12 };
+            yield return new object[] { a, res };
+
+            // 5: Древо глубины 3 и полностью заполненное
+            a = new int[] { 7, 1, 12, 5, 10, 3, 14, 8, 0, 6, 13, 2, 4, 9, 11 };
+            res = new int[] { 7, 3, 11, 1, 5, 9, 13, 0, 2, 4, 6, 8, 10, 12, 14 };
+            yield return new object[] { a, res };
+
+            // 6: Уже отсортированный
+            a = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            res = new int[] { 7, 3, 11, 1, 5, 9, 13, 0, 2, 4, 6, 8, 10, 12, 14 };
+            yield return new object[] { a, res };
+
+            // 7: Дерево из массива картинки предыдущего урока
+            a = new int[] { 50, 25, 75, 10, 30, 60, 90, 5, 15, 27, 35, 55, 65, 85, 95 };
+            res = new int[] { 50, 25, 75, 10, 30, 60, 90, 5, 15, 27, 35, 55, 65, 85, 95 };
             yield return new object[] { a, res };
         }
     }

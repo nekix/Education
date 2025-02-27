9cf3f9b531aeae8e756c513dc58c23fb5713c2bd
nekix <nekix@DESKTOP-7HDAA7P>
2025-02-27 18:08:41 +0300

    Реализовал основное задание десятого урока.

---
+++ Ads/Education.Ads.Tests/Exercise10/SimpleGraph_Tests.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmsDataStructures2;
+using Shouldly;
+using Xunit;
+
+namespace Education.Ads.Tests.Exercise10
+{
+    public class SimpleGraph_Tests
+    {
+        [Theory]
+        [MemberData(nameof(GetDepthFirstSearchData))]
+        public void Should_DepthFirstSearch(SimpleGraph<int> graph, int VFrom, int VTo, List<int> resultPath)
+        {
+            List<Vertex<int>> path = graph.DepthFirstSearch(VFrom, VTo);
+
+            path.Select(x => x.Value).ShouldBe(resultPath);
+        }
+
+        public static IEnumerable<object[]> GetDepthFirstSearchData()
+        {
+            SimpleGraph<int> graph;
+            int vFrom;
+            int vTo;
+            List<int> resultPath;
+
+            // 1. Проверка на границы в пустом графе
+            graph = new SimpleGraph(0);
+            vFrom = 0;
+            vTo = 0;
+            resultPath = new List<int>
+            {
+                
+            };
+            yield return new object[] { graph, vFrom, vTo, resultPath };
+
+            // 2. Проверка на поиск отрицательных узлом
+            graph = new SimpleGraph(6);
+            vFrom = -1;
+            vTo = -1;
+            resultPath = new List<int>
+            {
+
+            };
+            yield return new object[] { graph, vFrom, vTo, resultPath };
+
+            // 3. Проверка на поиск узлов за границами (больше) вместимости
+            graph = new SimpleGraph(6);
+            vFrom = 6;
+            vTo = 7;
+            resultPath = new List<int>
+            {
+
+            };
+            yield return new object[] { graph, vFrom, vTo, resultPath };
+
+            // 4. Проверка на поиск в пустом графе
+            graph = new SimpleGraph<int>(6);
+            vFrom = 0;
+            vTo = 2;
+            resultPath = new List<int>
+            {
+
+            };
+            yield return new object[] { graph, vFrom, vTo, resultPath };
+
+            // 5. Проверка на поиск c несуществующим узлом
+            graph = new SimpleGraph<int>(6);
+            vFrom = 0;
+            vTo = 2;
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+            graph.AddVertex(6);
+            graph.AddEdge(0, 4);
+            graph.AddEdge(1, 4);
+            graph.AddEdge(3, 2);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 4);
+            graph.AddEdge(2, 0);
+            graph.RemoveVertex(2);
+            resultPath = new List<int>
+            {
+
+            };
+            yield return new object[] { graph, vFrom, vTo, resultPath };
+
+            // 6. Простой поиск
+            graph = new SimpleGraph<int>(6);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+            graph.AddVertex(6);
+            graph.AddEdge(0, 4);
+            graph.AddEdge(1, 4);
+            graph.AddEdge(3, 2);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 4);
+            graph.AddEdge(2, 0);
+            vFrom = 0;
+            vTo = 2;
+            resultPath = new List<int>
+            {
+                2, 4
+            };
+            yield return new object[] { graph, vFrom, vTo, resultPath };
+
+            // 7. Поиск через промежуточный узел
+            graph = new SimpleGraph<int>(6);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+            graph.AddVertex(6);
+            graph.AddEdge(0, 4);
+            graph.AddEdge(1, 4);
+            graph.AddEdge(3, 2);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 4);
+            //graph.AddEdge(2, 0);
+            vFrom = 0;
+            vTo = 2;
+            resultPath = new List<int>
+            {
+                2, 6, 4
+            };
+            yield return new object[] { graph, vFrom, vTo, resultPath };
+
+            // 8. Проверка на поиск с циклами
+            graph = new SimpleGraph<int>(6);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+            graph.AddVertex(6);
+            graph.AddEdge(0, 4);
+            graph.AddEdge(3, 4);
+            graph.AddEdge(3, 0);
+            graph.AddEdge(3, 2);
+            //graph.AddEdge(2, 0);
+            vFrom = 0;
+            vTo = 2;
+            resultPath = new List<int>
+            {
+                2, 5, 4
+            };
+            yield return new object[] { graph, vFrom, vTo, resultPath };
+
+            // 9. Проверка на отсутсвие пути с двумя циклами
+            graph = new SimpleGraph<int>(6);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+            graph.AddVertex(6);
+            graph.AddVertex(7);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(3, 4);
+            graph.AddEdge(4, 5);
+            graph.AddEdge(3, 5);
+            //graph.AddEdge(2, 0);
+            vFrom = 0;
+            vTo = 4;
+            resultPath = new List<int>
+            {
+                
+            };
+            yield return new object[] { graph, vFrom, vTo, resultPath };
+
+            // 10. Длинный путь
+            graph = new SimpleGraph<int>(6);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+            graph.AddVertex(6);
+            graph.AddVertex(7);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 4);
+            graph.AddEdge(4, 5);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(4, 2);
+            //graph.AddEdge(2, 0);
+            vFrom = 0;
+            vTo = 5;
+            resultPath = new List<int>
+            {
+                2, 3, 4, 5, 6, 7
+            };
+            yield return new object[] { graph, vFrom, vTo, resultPath };
+        }
+    }
+}
---
+++ Ads/Education.Ads/Exercise8_10/SimpleGraph.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class Vertex<T>
+    {
+        public bool Hit;
+        public T Value;
+        public Vertex(T val)
+        {
+            Value = val;
+            Hit = false;
+        }
+    }
+
+    public class Vertex : Vertex<int>
+    {
+        public Vertex(int val) : base(val)
+        {
+        }
+    }
+
+    public class SimpleGraph<T>
+    {
+        public Vertex<T>[] vertex;
+        public int[,] m_adjacency;
+        public int max_vertex;
+
+        public SimpleGraph(int size)
+        {
+            max_vertex = size;
+            m_adjacency = new int[size, size];
+            vertex = new Vertex<T>[size];
+        }
+
+        public void AddVertex(T value)
+        {
+            int slot = Array.IndexOf(vertex, null);
+
+            if (slot != -1)
+                vertex[slot] = new Vertex<T>(value);
+        }
+
+        public void RemoveVertex(int v)
+        {
+            if (!IsInRange(v))
+                return;
+
+            for (int i = 0; i < max_vertex; i++)
+            {
+                m_adjacency[v, i] = 0;
+                m_adjacency[i, v] = 0;
+            }
+
+            vertex[v] = null;
+        }
+
+        public bool IsEdge(int v1, int v2)
+        {
+            if (!(IsInRange(v1) && IsInRange(v2)))
+                return false;
+
+            if (vertex[v1] == null || vertex[v2] == null)
+                return false;
+
+            return m_adjacency[v1, v2] == 1;
+        }
+
+        public virtual void AddEdge(int v1, int v2)
+        {
+            if (!(IsInRange(v1) && IsInRange(v2)))
+                return;
+
+            if (vertex[v1] == null || vertex[v2] == null)
+                return;
+
+            m_adjacency[v1, v2] = 1;
+            m_adjacency[v2, v1] = 1;
+        }
+
+        public virtual void RemoveEdge(int v1, int v2)
+        {
+            if (!(IsInRange(v1) && IsInRange(v2)))
+                return;
+
+            m_adjacency[v1, v2] = 0;
+            m_adjacency[v2, v1] = 0;
+        }
+
+        public List<Vertex<T>> DepthFirstSearch(int VFrom, int VTo)
+        {
+            if (!IsInRange(VFrom) || !IsInRange(VTo))
+                return new List<Vertex<T>>(0);
+
+            if (vertex[VFrom] == null || vertex[VTo] == null)
+                return new List<Vertex<T>>(0);
+
+            // 0: Cleared
+            Stack<int> path = new Stack<int>();
+
+            foreach (Vertex<T> item in vertex)
+                if (item != null)
+                    item.Hit = false;
+
+            // 1: Select the current vertex
+            int current = VFrom;
+            while (true)
+            {
+                // 4.2.2 No unvisited vertices left
+                // Go to step 5.
+                if (vertex[current].Hit)
+                {
+                    // 5. Remove the top node from the stack
+                    path.Pop();
+
+                    // 5.1 Stack is empty, exit
+                    if (path.Count == 0)
+                        break;
+
+                    // 5.2 Take the new top node as the current one
+                    current = path.Peek();
+                }
+                else
+                {
+                    // 2: Mark as visited
+                    vertex[current].Hit = true;
+
+                    // 3: Put on the stack
+                    path.Push(current);
+
+                    // 4: Check for the presence of the target vertex
+                    if (IsEdge(current, VTo))
+                    {
+                        // 4.1 Return the result
+                        path.Push(VTo);
+                        break;
+                    }
+                }
+
+                // 4.2 Find the next unvisited vertex
+                for (int i = 0; i < vertex.Length; i++)
+                {
+                    if (IsEdge(current, i) && vertex[i].Hit == false)
+                    {
+                        // 4.2.1 Take this as the current
+                        // and go to step 2
+                        current = i;
+                        break;
+                    }
+                }
+            }
+
+            List<Vertex<T>> vertexPath = StackPathToVertexList(path);
+
+            return vertexPath;
+        }
+
+        protected bool IsInRange(int index)
+        {
+            if (index < 0 || index >= vertex.Length)
+                return false;
+
+            return true;
+        }
+
+        private List<Vertex<T>> StackPathToVertexList(Stack<int> path)
+        {
+            List<Vertex<T>> result = new List<Vertex<T>>(path.Count);
+            while (path.Count != 0)
+            {
+                int index = path.Pop();
+                result.Add(vertex[index]);
+            }
+
+            ReverseList(result);
+
+            return result;
+        }
+
+        private void ReverseList(List<Vertex<T>> result)
+        {
+            for (int i = 0, j = result.Count - 1; i < j; i++, j--)
+                (result[i], result[j]) = (result[j], result[i]);
+        }
+    }
+
+    public class SimpleGraph : SimpleGraph<int>
+    {
+        public SimpleGraph(int size) : base(size)
+        {
+        }
+    }
+}

4a40d0cea8841e8bd9319d570e7a2d9ed26bb42e
nekix <nekix@DESKTOP-7HDAA7P>
2025-03-26 08:19:40 +0300

    Привел АСД 2 к шаблону решения уроков.

--- Ads/Education.Ads/Exercise8_10_11_12/SimpleGraph-2.cs
+++ Ads/Education.Ads/Exercise8_10_11_12/SimpleGraph-2.cs
@@ -5,6 +5,74 @@ namespace AlgorithmsDataStructures2
 {
     // ========= Урок 8 ===========
 
+    // Задание 2.
+    // Реализуйте направленный граф, представленный матрицей смежности. 
+    // Добавить метод проверки на цикличность.
+    // Направленный граф реализовал наследованием от простого графа,
+    // только переопределил соответствующие методы AddEdge и RemoveEdge.
+    // Сложности методов остались такими же.
+    public partial class DirectedGraph : SimpleGraph
+    {
+        private const int NotVisisted = 0;
+        private const int Visited = 1;
+        private const int Verified = 2;
+
+        public DirectedGraph(int size) : base(size)
+        {
+        }
+
+        public override void AddEdge(int v1, int v2)
+        {
+            if (!(IsInRange(v1) && IsInRange(v2)))
+                return;
+
+            if (vertex[v1] == null || vertex[v2] == null)
+                return;
+
+            m_adjacency[v1, v2] = 1;
+        }
+
+        public override void RemoveEdge(int v1, int v2)
+        {
+            if (!(IsInRange(v1)))
+                return;
+
+            m_adjacency[v1, v2] = 0;
+        }
+
+        public bool CheckIsCyclical()
+        {
+            byte[] vertexes = new byte[vertex.Length];
+
+            for (int v1 = 0; v1 < vertex.Length; v1++)
+            {
+                if (vertexes[v1] == NotVisisted && CheckIsCyclical(v1, vertexes))
+                    return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckIsCyclical(int v1, byte[] vertexes)
+        {
+            vertexes[v1] = Visited;
+
+            for (int v2 = 0; v2 < vertex.Length; v2++)
+            {
+                if (IsEdge(v1, v2))
+                {
+                    if (vertexes[v2] == Visited)
+                        return true;
+                    else if (vertexes[v2] == NotVisisted && CheckIsCyclical(v2, vertexes))
+                        return true;
+                }
+            }
+
+            vertexes[v1] = Verified;
+
+            return false;
+        }
+    }
 
     // ========================================================================
 
@@ -121,6 +189,16 @@ namespace AlgorithmsDataStructures2
             return maxPathCount;
         }
     }
+
+    // ========================================================================
+
+    // Рефлексия по уроку 8.
+
+    // Задание 2.
+    // Реализуйте направленный граф, представленный матрицей смежности. 
+    // Добавить метод проверки на цикличность.
+    // Можно было можно было не рассматривать петли (рёбра из вершины в себя).
+    // Можно было бы взять только верхнюю или нижнюю треугольную часть матрицы.
 
     // ========= Урок 11 ===========
 

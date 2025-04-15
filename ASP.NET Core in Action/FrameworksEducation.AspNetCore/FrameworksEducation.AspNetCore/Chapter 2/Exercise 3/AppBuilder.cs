85778f09bcf465194947d8c5cee22267a68fce61
npetrov <npetrov@advmusic.com>
2025-04-15 18:02:32 +0300

    Вечерний update по ASP.NET Core.

---
+++ ASP.NET Core in Action/FrameworksEducation.AspNetCore/FrameworksEducation.AspNetCore/Chapter 2/Exercise 3/AppBuilder.cs
@@ -0,0 +1,64 @@
+﻿using Yarp.ReverseProxy.Configuration;
+
+namespace FrameworksEducation.AspNetCore.Chapter_2.Exercise_3;
+
+public static class AppBuilder
+{
+    public static WebApplication BuildAndConfigureLegacyApp()
+    {
+
+    }
+
+    public static WebApplication BuildAndConfigureModernApp(string[] args)
+    {
+        WebApplicationOptions options = new WebApplicationOptions
+        {
+            Args = args
+        };
+
+        WebApplicationBuilder builder = WebApplication.CreateEmptyBuilder(options);
+
+        builder.WebHost.UseKestrelCore();
+        builder.WebHost.UseUrls("http://localhost:5005");
+
+        // Configure logging
+        builder.Logging.AddConsole();
+        builder.Services.AddHttpLogging(opts =>
+            opts.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProperties);
+        builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+
+        List<RouteConfig> routes = new List<RouteConfig>()
+        {
+            new RouteConfig
+            {
+                RouteId = "route1",
+                ClusterId = "cluster1",
+                Match = new RouteMatch
+                {
+                    Path = "{**catch-all}"
+                }
+            }
+        };
+        List<ClusterConfig> clusters = new List<ClusterConfig>()
+        {
+            new ClusterConfig
+            {
+                ClusterId = "cluster1",
+                Destinations = new Dictionary<string, DestinationConfig>
+                {
+
+                }
+            }
+        };
+
+        builder.Services.AddReverseProxy().LoadFromMemory(routes, clusters);
+
+        WebApplication app = builder.Build();
+
+        app.Use(async (context, next) =>
+        {
+            await context.Response.WriteAsync("Hello World!");
+            await next(context);
+        });
+    }
+}

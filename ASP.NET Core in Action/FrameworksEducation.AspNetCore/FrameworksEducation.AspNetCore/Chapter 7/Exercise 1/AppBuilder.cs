3c66381f4ef228c3ab0b464b4b20c0ff4360b90b
nekix <nekix@DESKTOP-7HDAA7P>
2025-04-21 08:10:08 +0300

    Главы 8 и 9. Частично выполнено задание для 9-й главы.

--- ASP.NET Core in Action/FrameworksEducation.AspNetCore/FrameworksEducation.AspNetCore/Chapter 7/Exercise 1/AppBuilder.cs
+++ ASP.NET Core in Action/FrameworksEducation.AspNetCore/FrameworksEducation.AspNetCore/Chapter 7/Exercise 1/AppBuilder.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Xml.Linq;
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FrameworksEducation.AspNetCore.Chapter_7.Exercise_1;
---
+++ ASP.NET Core in Action/FrameworksEducation.AspNetCore/FrameworksEducation.AspNetCore/Chapter 9/Exercise 1/AppBuilder.cs
@@ -0,0 +1,74 @@
+﻿using System.Security.Cryptography;
+
+namespace FrameworksEducation.AspNetCore.Chapter_9.Exercise_1;
+
+public class AppBuilder
+{
+    public static WebApplication Configure(string[] args)
+    {
+        WebApplicationOptions options = new WebApplicationOptions
+        {
+            Args = args
+        };
+
+        WebApplicationBuilder builder = WebApplication.CreateEmptyBuilder(options);
+
+        builder.WebHost.UseKestrelCore();
+        builder.WebHost.UseUrls("http://localhost:5005");
+
+        builder.Services.AddRoutingCore();
+        builder.Services.AddProblemDetails();
+
+        // 1. Отработал ColoredConsoleWriter Singleton
+        builder.Services.AddScoped<IConsoleWriter, SimpleConsoleWriter>();
+        builder.Services.AddSingleton<IConsoleWriter, ColoredConsoleWriter>();
+
+        WebApplication app = builder.Build();
+
+        app.UseRouting();
+
+        app.MapGet("di-test/", (IConsoleWriter writer) =>
+        {
+            writer.WriteToConsole("Invoked 'di-test' handler!");
+
+            return Results.Ok();
+        });
+
+        return app;
+    }
+}
+
+public interface IConsoleWriter
+{
+    void WriteToConsole(string message);
+}
+
+public class SimpleConsoleWriter : IConsoleWriter
+{
+    private readonly int _id;
+
+    public SimpleConsoleWriter()
+    {
+        _id = new Random().Next();
+    }
+
+    public void WriteToConsole(string message)
+    {
+        Console.WriteLine($"{_id}:{message}");
+    }
+}
+
+public class ColoredConsoleWriter : IConsoleWriter
+{
+    private readonly int _id;
+
+    public ColoredConsoleWriter()
+    {
+        _id = new Random().Next();
+    }
+
+    public void WriteToConsole(string message)
+    {
+        Console.WriteLine($"{_id}:colored message: {message}");
+    }
+}

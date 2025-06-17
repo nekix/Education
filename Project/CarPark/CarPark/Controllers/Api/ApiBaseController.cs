758dfd7ff08e624d9fbab1a3f99194cfbe83a69e
nekix <nekix@DESKTOP-7HDAA7P>
2025-06-17 08:08:09 +0300

    Установка "application/json" в качестве формата возвращаемых значений для API.

---
+++ Project/CarPark/CarPark/Controllers/Api/ApiBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarPark.Controllers.Api;
+
+[Route("api/[controller]")]
+[ApiController]
+[Produces("application/json")]
+public abstract class ApiBaseController : ControllerBase
+{
+}
--- Project/CarPark/CarPark/Controllers/Api/VehiclesController.cs
+++ Project/CarPark/CarPark/Controllers/Api/VehiclesController.cs
@@ -4,113 +4,110 @@ using CarPark.Data;
 using CarPark.Models;
 using Microsoft.AspNetCore.Http;
 
-namespace CarPark.Controllers.Api
+namespace CarPark.Controllers.Api;
+
+public class VehiclesController : ApiBaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class VehiclesController : ControllerBase
+    private readonly ApplicationDbContext _context;
+
+    public VehiclesController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public VehiclesController(ApplicationDbContext context)
+    // GET: api/VehiclesApi
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Vehicle>>> GetVehicles()
+    {
+        return await _context.Vehicles.ToListAsync();
+    }
+
+    // GET: api/VehiclesApi/5
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<Vehicle>> GetVehicle(int id)
+    {
+        var vehicle = await _context.Vehicles.FindAsync(id);
+
+        if (vehicle == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: api/VehiclesApi
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Vehicle>>> GetVehicles()
+        return vehicle;
+    }
+
+    // PUT: api/VehiclesApi/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<IActionResult> PutVehicle(int id, Vehicle vehicle)
+    {
+        if (id != vehicle.Id)
         {
-            return await _context.Vehicles.ToListAsync();
+            return BadRequest();
         }
 
-        // GET: api/VehiclesApi/5
-        [HttpGet("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesDefaultResponseType]
-        public async Task<ActionResult<Vehicle>> GetVehicle(int id)
-        {
-            var vehicle = await _context.Vehicles.FindAsync(id);
+        _context.Entry(vehicle).State = EntityState.Modified;
 
-            if (vehicle == null)
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!VehicleExists(id))
             {
                 return NotFound();
             }
-
-            return vehicle;
-        }
-
-        // PUT: api/VehiclesApi/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesDefaultResponseType]
-        public async Task<IActionResult> PutVehicle(int id, Vehicle vehicle)
-        {
-            if (id != vehicle.Id)
+            else
             {
-                return BadRequest();
+                throw;
             }
-
-            _context.Entry(vehicle).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!VehicleExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
         }
 
-        // POST: api/VehiclesApi
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesDefaultResponseType]
-        public async Task<ActionResult<Vehicle>> PostVehicle(Vehicle vehicle)
+        return NoContent();
+    }
+
+    // POST: api/VehiclesApi
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<Vehicle>> PostVehicle(Vehicle vehicle)
+    {
+        _context.Vehicles.Add(vehicle);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetVehicle", new { id = vehicle.Id }, vehicle);
+    }
+
+    // DELETE: api/VehiclesApi/5
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<IActionResult> DeleteVehicle(int id)
+    {
+        var vehicle = await _context.Vehicles.FindAsync(id);
+        if (vehicle == null)
         {
-            _context.Vehicles.Add(vehicle);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetVehicle", new { id = vehicle.Id }, vehicle);
+            return NotFound();
         }
 
-        // DELETE: api/VehiclesApi/5
-        [HttpDelete("{id}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesDefaultResponseType]
-        public async Task<IActionResult> DeleteVehicle(int id)
-        {
-            var vehicle = await _context.Vehicles.FindAsync(id);
-            if (vehicle == null)
-            {
-                return NotFound();
-            }
+        _context.Vehicles.Remove(vehicle);
+        await _context.SaveChangesAsync();
 
-            _context.Vehicles.Remove(vehicle);
-            await _context.SaveChangesAsync();
+        return NoContent();
+    }
 
-            return NoContent();
-        }
-
-        private bool VehicleExists(int id)
-        {
-            return _context.Vehicles.Any(e => e.Id == id);
-        }
+    private bool VehicleExists(int id)
+    {
+        return _context.Vehicles.Any(e => e.Id == id);
     }
 }

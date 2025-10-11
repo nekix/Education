2888b926f537a215222faf0e35093740f9b30b92
nekix <nekix@DESKTOP-7HDAA7P>
2025-10-11 12:54:16 +0300

    Миграция для корректировки неучтенных изменений

---
+++ Project/CarPark/CarPark.Infrastructure/Data/Migrations/20251010173710_FixMigrationsDiffs.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarPark.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMigrationsDiffs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_enterprsise_manager_manager_managers_id",
+                schema: "car_park",
+                table: "enterprise_manager");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "enterprise_id",
+                schema: "car_park",
+                table: "vehicle",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "enterprise_id",
+                schema: "car_park",
+                table: "driver",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "model",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "time_zone",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "manager",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "enterprise",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "model_id",
+                schema: "car_park",
+                table: "vehicle",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "vehicle",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "added_to_enterprise_at",
+                schema: "car_park",
+                table: "vehicle",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: DateTimeOffset.MinValue);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "driver",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "assigned_drivers_id",
+                schema: "car_park",
+                table: "driver_vehicle_assignment",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "assigned_vehicles_id",
+                schema: "car_park",
+                table: "driver_vehicle_assignment",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "vehicle_id",
+                schema: "car_park",
+                table: "vehicle_geo_time_point",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "vehicle_id",
+                schema: "car_park",
+                table: "ride",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "enterprises_id",
+                schema: "car_park",
+                table: "enterprise_manager",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            // 4. Добавляем primary keys для join-таблиц
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_driver_vehicle_assignment",
+                schema: "car_park",
+                table: "driver_vehicle_assignment",
+                columns: new[] { "assigned_drivers_id", "assigned_vehicles_id" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_enterprise_manager",
+                schema: "car_park",
+                table: "enterprise_manager",
+                columns: new[] { "enterprises_id", "managers_id" });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_driver_enterprise_id",
+                schema: "car_park",
+                table: "driver",
+                column: "enterprise_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_driver_vehicle_assignment_assigned_vehicles_id",
+                schema: "car_park",
+                table: "driver_vehicle_assignment",
+                column: "assigned_vehicles_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_vehicle_geo_time_point_vehicle_id",
+                schema: "car_park",
+                table: "vehicle_geo_time_point",
+                column: "vehicle_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_ride_vehicle_id",
+                schema: "car_park",
+                table: "ride",
+                column: "vehicle_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_ride_vehicle_id",
+                schema: "car_park",
+                table: "ride");
+
+            migrationBuilder.DropIndex(
+                name: "ix_vehicle_geo_time_point_vehicle_id",
+                schema: "car_park",
+                table: "vehicle_geo_time_point");
+
+            migrationBuilder.DropIndex(
+                name: "ix_driver_vehicle_assignment_assigned_vehicles_id",
+                schema: "car_park",
+                table: "driver_vehicle_assignment");
+
+            migrationBuilder.DropIndex(
+                name: "ix_driver_enterprise_id",
+                schema: "car_park",
+                table: "driver");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_enterprise_manager",
+                schema: "car_park",
+                table: "enterprise_manager");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_driver_vehicle_assignment",
+                schema: "car_park",
+                table: "driver_vehicle_assignment");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "enterprises_id",
+                schema: "car_park",
+                table: "enterprise_manager",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "vehicle_id",
+                schema: "car_park",
+                table: "ride",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "vehicle_id",
+                schema: "car_park",
+                table: "vehicle_geo_time_point",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "assigned_vehicles_id",
+                schema: "car_park",
+                table: "driver_vehicle_assignment",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "assigned_drivers_id",
+                schema: "car_park",
+                table: "driver_vehicle_assignment",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "driver",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "enterprise_id",
+                schema: "car_park",
+                table: "driver",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "added_to_enterprise_at",
+                schema: "car_park",
+                table: "vehicle",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: DateTimeOffset.MinValue,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "vehicle",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "model_id",
+                schema: "car_park",
+                table: "vehicle",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "enterprise_id",
+                schema: "car_park",
+                table: "vehicle",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "enterprise",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "manager",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "time_zone",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "car_park",
+                table: "model",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_enterprsise_manager_manager_managers_id",
+                schema: "car_park",
+                table: "enterprise_manager",
+                column: "managers_id",
+                principalSchema: "car_park",
+                principalTable: "manager",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
